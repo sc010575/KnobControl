3f5ebf8de7d24d894238fefd477f8260cfc42f61
Suman Chatterjee <chatterjeesuman@gmail.com>
2018-12-25 08:59:41 +0530

    Initial commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Suman Chatterjee on 24/12/2018.
+//  Copyright © 2018 Suman Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
